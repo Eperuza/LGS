ec4fda1349a36a0bb938245d4fc3065a1cb431fc
Eperuza <jeremy.riv@outlook.com>
2021-08-02 14:21:15 -0400

    api setup

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
-const express = require ('express');
-const app = express();
-const port = 8080;
+require('dotenv').config();
+const app = require('./app.js');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
