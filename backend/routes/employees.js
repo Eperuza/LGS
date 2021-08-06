1a67ef9657c811411293489f634bd465a07b318d
Eperuza <jeremy.riv@outlook.com>
2021-08-06 13:28:26 -0400

    customer and employee endoints complete

--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -3,7 +3,7 @@ const router = express.Router();
 require('dotenv').config();
 const knex = require('knex')(require('../knexfile.js')[process.env.ENVIRONMENT])
 
-//returns all customers
+//returns all employees
 router.get('/', (req, res) => {
   knex.select('*')
   .from('employees')
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
   .catch('An error occured')
 })
 
-//returns a customer by ID
+//returns an employee by ID
 router.get('/:employeeId', (req, res) => {
   const employeeId = req.params.employeeId
   knex.select('*')
@@ -19,6 +19,32 @@ router.get('/:employeeId', (req, res) => {
   .where({id: employeeId})
   .then(data => res.status(200).json(data))
   .catch('An error occured')
+})
+
+//create an employee
+router.post('/', (req, res) => {
+  const newEmployee = req.body;
+  knex('employees')
+  .insert({...newEmployee})
+  .then(data => res.send('employee created').json(newEmployee));
+})
+
+//delete an employee
+router.delete('/:employeeId', (req, res) => {
+  const toDelete = parseInt(req.params.employeeId)
+  knex('employees')
+  .where({id: toDelete})
+  .del()
+  .then(data => res.send('Employee succesfully deleted'))
+})
+
+//update an employee
+router.put('/:employeeId', (req, res) => {
+  const toUpdate= parseInt(req.params.employeeId)
+  knex('employees')
+  .where({id: toUpdate})
+  .update(req.body)
+  .then(data => res.send('Employee succesfully updated'))
 })
 
 module.exports = router;
