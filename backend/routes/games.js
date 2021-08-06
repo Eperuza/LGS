ef64d6a34809df55c2e79fe1f9336d04ea6fc765
Eperuza <jeremy.riv@outlook.com>
2021-08-06 14:05:29 -0400

    Only transactions endpoint to do still. Going to new branch

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -3,11 +3,35 @@ const router = express.Router();
 require('dotenv').config();
 const knex = require('knex')(require('../knexfile.js')[process.env.ENVIRONMENT])
 
-
+//return all games
 router.get('/', (req, res) => {
   knex.select('*')
   .from('games')
   .then(data => res.status(200).json(data))
+})
+
+//return game by ID
+router.get('/:gameId', (req, res) => {
+  const game = parseInt(req.params.gameId);
+  knex.select('*')
+  .from('games')
+  .where({id: game})
+  .then(data => res.status(200).json(data))
+})
+
+game.put('/:gameId', (req, res) => {
+  const toUpdate = parseInt(req.params.gameId)
+  knex('games')
+  .where({id: toUpdate})
+  .update(req.body)
+  .then(data => res.status(202).send('Game succesfully updated'))
+})
+
+game.post('/new', (req, res) => {
+  const newGame = req.body;
+  knex('games')
+  .insert(newGame)
+  .then(data => res.status(201).send('Game succesfully created'))
 })
 
 module.exports = router;
